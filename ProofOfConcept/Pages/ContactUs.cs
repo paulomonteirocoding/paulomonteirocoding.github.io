fd053a6b23e43d71ef983b7afd37aa2aa198af21
learning-blazor-and-other-stuff <pcmfm@hotmail.com>
2020-09-13 16:16:30 +0100

    .

---
+++ ProofOfConcept/Pages/ContactUs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProofOfConcept.Pages
+{
+    public partial class ContactUs
+    {
+
+        public string name { get; set; } = string.Empty;
+        public string capturedName { get; set; } = string.Empty;
+
+        public void getFormData()
+        {
+            capturedName = "Hello " + name;
+        }
+
+    }
+}
---
+++ ProofOfConcept/Pages/Index.cs
@@ -0,0 +1,11 @@
+﻿namespace ProofOfConcept.Pages
+{
+    public partial class Index 
+    {
+        int counter = 0;
+
+        public void increaseCounter() { counter++; }
+        public void decreaseCounter() { counter--; }
+
+    }
+}
