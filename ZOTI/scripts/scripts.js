594040d24db10013005a0a5db6e15732f2a8d6ed
paulomonteirocoding <71183565+paulomonteirocoding@users.noreply.github.com>
2021-02-02 10:25:44 +0000

    Update scripts.js

--- ZOTI/scripts/scripts.js
+++ ZOTI/scripts/scripts.js
@@ -112,22 +112,31 @@ function toggleMenuVisibility(){
     }
 }
 
-var slideClassName = "mySlides";
-var slidesCount = document.getElementsByClassName(slideId).length - 1;
-var slideIndex = 0;
-showSlides(0);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function plusSlides(no) {
-    showSlides(slideIndex+= n, no);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showSlides(no) {
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName(slideId).length;
-  if (no > x) {slideIndex = slidesCount - 1}    
-  if (no < 0) {slideIndex = 0}
-  for (i = 0; i < x ; i++) {
-     x[i].style.display = "none";  
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  x[slideIndex].style.display = "block";  
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
