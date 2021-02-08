df64724aba40170889617fd6694cc0417f2ef539
learning-blazor-and-other-stuff <pcmfm@hotmail.com>
2021-02-08 00:23:25 +0000

    final touches

--- ZOTI/scripts/scripts.js
+++ ZOTI/scripts/scripts.js
@@ -9,3 +9,28 @@ function toggleMenuVisibility(){
 
     }
 }
+
+
+document.getElementById("sendWhatsappMessageBtn").addEventListener('click', ()=>{
+    var urlToSendMessage = "https://wa.me/351938556463?text=" + encodeURI(document.getElementById("message").value);
+    window.open(urlToSendMessage);
+    document.getElementById("message").value = "";
+
+});
+
+window.addEventListener('resize', setiframesize());
+
+function setiframesize(){
+    var iframes = document.getElementsByTagName("iframe");
+    
+    if(window.innerWidth < 400){
+        
+        for(i = 0; i< iframes.length ; i++){
+            iframes[i].width = 300;
+        }
+    }else{
+        for(i = 0; i< iframes.length ; i++){
+            iframes[i].width = 350;
+        }
+    }
+}
