3ffe6ea8a8a3700559fc1b0cbab7ed7a25914e21
learning-blazor-and-other-stuff <pcmfm@hotmail.com>
2021-03-06 18:05:48 +0000

    xfgn

--- ZOTI/scripts/scripts.js
+++ ZOTI/scripts/scripts.js
@@ -1,7 +1,7 @@
 document.getElementById("menu-toggle").addEventListener('click',toggleMenuVisibility);
 
 function toggleMenuVisibility(){
-    if(window.innerWidth < 900 || document.getElementById("menu").classList.contains("visible")){
+    if(window.innerWidth <= 900 || document.getElementById("menu").classList.contains("visible")){
         
         document.getElementById("menu").classList.toggle("visible");
         document.getElementById("menu-close").firstChild.classList.toggle("fa-3x");
