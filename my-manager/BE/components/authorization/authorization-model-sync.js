afa8f7bd1254f28d7ef3ea17a05f35ac8cb8a866
paulomonteirocoding <71183565+paulomonteirocoding@users.noreply.github.com>
2025-07-16 01:39:28 +0100

    qqqqq

--- my-manager/BE/components/authorization/authorization-model-sync.js
+++
@@ -1,5 +0,0 @@
-require('./models/permission-model').sync();
-require('./models/role-model').sync();
-require('./models/role-permissions-model').sync();
-require('./models/user-permissions-model').sync();
-require('./models/user-roles-model').sync();
